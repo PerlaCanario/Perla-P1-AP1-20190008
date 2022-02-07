e0640fda12041a529b629977150bc5cbef96dfc2
Perla D. Canario Perez <perladenis163@gmail.com>
2022-02-07 18:12:16 -0400

    agregando consultas y registros

---
+++ UI/Consultas/cAportes.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PerlaD_P1_AP1_20190008.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cAportes.xaml
+    /// </summary>
+    public partial class cAportes : Window
+    {
+        public cAportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
