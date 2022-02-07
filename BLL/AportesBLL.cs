2125417810ae1e3a9782492ac9584590b3c07572
Perla D. Canario Perez <perladenis163@gmail.com>
2022-02-07 17:42:38 -0400

    Agregando BLL

---
+++ BLL/AportesBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerlaD_P1_AP1_20190008.BLL
+{
+    class AportesBLL
+    {
+    }
+}
