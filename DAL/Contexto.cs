5342807637c6a44768de451a47487d717306515c
Perla D. Canario Perez <perladenis163@gmail.com>
2022-02-07 17:21:18 -0400

    Agregando contexto y instalando Entity Framework

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PerlaD_P1_AP1_20190008.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerlaD_P1_AP1_20190008.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\DBP1-PerlaD.-20190008.db");
+        }
+    }
+}
