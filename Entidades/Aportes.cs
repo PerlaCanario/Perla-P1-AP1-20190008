b50df2c1472d38926a5895f37e87099fa0b4350b
Perla D. Canario Perez <perladenis163@gmail.com>
2022-02-07 17:10:35 -0400

    Agregando Entidades

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerlaD_P1_AP1_20190008.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AportesId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+        public float Monto { get; set; }
+    }
+}
