f37ecc59b95cc41be088cfffb90965330a30a01a
Perla D. Canario Perez <perladenis163@gmail.com>
2022-02-07 20:22:45 -0400

    Programando botones

--- UI/Consultas/cAportes.xaml.cs
+++ UI/Consultas/cAportes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PerlaD_P1_AP1_20190008.BLL;
+using PerlaD_P1_AP1_20190008.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,5 +25,43 @@ namespace PerlaD_P1_AP1_20190008.UI.Consultas
         {
             InitializeComponent();
         }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Aportes>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = AportesBLL.GetList(e => e.Persona.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+
+                    case 1:
+                        listado = AportesBLL.GetList(e => e.Descripcion.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = AportesBLL.GetList(c => true);
+            }
+            if (DesdeDatePicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date >= DesdeDatePicker.SelectedDate);
+
+            if (HastaDataPicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date <= HastaDataPicker.SelectedDate);
+
+            AportesDataGrid.ItemsSource = null;
+            AportesDataGrid.ItemsSource = listado;
+
+            var Conteo = listado.Count();
+            ConteoTextBox.Text = Conteo.ToString();
+
+            var Total = listado.Sum(x => x.Monto);
+            TotalTextBox.Text = Total.ToString();
+        }
+
     }
 }
--- UI/Registros/rAportes.xaml.cs
+++ UI/Registros/rAportes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PerlaD_P1_AP1_20190008.BLL;
+using PerlaD_P1_AP1_20190008.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,86 @@ namespace PerlaD_P1_AP1_20190008.UI.Registros
     /// </summary>
     public partial class rAportes : Window
     {
+        private Aportes aportes;
         public rAportes()
         {
             InitializeComponent();
+            aportes = new Aportes();
+            this.DataContext = aportes;
+        }
+        private void Limpiar()
+        {
+            this.aportes = new Aportes();
+            this.DataContext = aportes;
+        }
+        public void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+        private bool Validar()
+        {
+            bool esValido = true;
+            if (AportesBLL.Existe(PersonaTextBox.Text))
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, no puede repetir el nombre", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+; if (PersonaTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, debe insertar un nombre", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            if (PersonaTextBox.Text.Any(char.IsDigit))
+            {
+                esValido = false;
+                MessageBox.Show("Ha ocurrido un error, no hay ciudades con numeros", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return esValido;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ciudad = AportesBLL.Buscar(Convert.ToInt32(AportesIdTextBox.Text));
+            if (ciudad != null)
+                this.aportes = ciudad;
+            else
+            {
+                this.aportes = new Aportes();
+                MessageBox.Show("No se ha encontrado", "Error",
+                   MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            this.DataContext = this.aportes;
+        }
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!Validar())
+                return;
+            var paso = AportesBLL.Guardar(this.aportes);
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Transaccion exitosa", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Transaccion fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AportesBLL.Eliminar(Convert.ToInt32(AportesIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
         }
     }
 }
